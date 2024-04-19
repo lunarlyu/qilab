7c93b7905799da00e4944370be0f1933e5ed6fc1
lunarlyu <luna.lyu.academic@gmail.com>
2024-04-19 00:36:02 -0700

    i

--- transfer-search-engine/src/App.js
+++ transfer-search-engine/src/App.js
@@ -3,7 +3,7 @@ import Explorer from './Explorer';
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,7 +20,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 
 const fakeData = [
