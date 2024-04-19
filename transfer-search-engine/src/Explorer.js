d1a45c5424154d65e590e7370801a214c48da75d
Luna <luna@DNa80b84f.SUNet>
2024-04-19 00:00:31 -0700

    init

---
+++ transfer-search-engine/src/Explorer.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+const Explorer = ({ data }) => {
+  const [openItem, setOpenItem] = useState(null);
+
+  const handleItemClick = (item) => {
+    setOpenItem(item.children ? item : null);
+  };
+
+  const renderItem = (item) => (
+    <div key={item.id} className="item" onClick={() => handleItemClick(item)}>
+      {/* Display item name with an icon based on its type */}
+      {item.type === 'folder' && <i className="fa fa-folder"></i>}
+      {item.type === 'file' && <i className="fa fa-file"></i>}
+      <span className="item-name">{item.name}</span>
+      {/* Conditionally display nested item count if available */}
+      {item.children && (
+        <span className="item-count">({item.children.length})</span>
+      )}
+      {/* Recursively render nested items if present */}
+      {item.children && openItem === item && (
+        <ul className="nested-list">
+          {item.children.map(renderItem)}
+        </ul>
+      )}
+    </div>
+  );
+
+  return (
+    <div className="explorer">
+      <ul className="explorer-list">
+        {data.map(renderItem)}
+      </ul>
+    </div>
+  );
+};
+
+export default Explorer;
